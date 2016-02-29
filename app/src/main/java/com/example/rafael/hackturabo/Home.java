87922822c56daf0e4b5568bf13099d8fd1c087b5
Jonathan <montanezj92@gmail.com>
2016-02-29 15:58:05 -0400

    Home Button

--- app/src/main/java/com/example/rafael/hackturabo/Home.java
+++ app/src/main/java/com/example/rafael/hackturabo/Home.java
@@ -1,7 +1,12 @@
 package com.example.rafael.hackturabo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Home extends ActionBarActivity {
 
@@ -9,6 +14,13 @@ public class Home extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
 
+        Button btnTest = (Button)findViewById(R.id.imageButton);
+        btnTest.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        startActivity(new Intent(Home.this, Menu.class));
+                    }
+                });
+    }
 }
